334c0a8bc3017e4687c4764189c6f63350f688d7
Wanqi Zhu <1213.ghs@gmail.com>
2018-04-11 10:00:26 -0700

    built without errors with grunt

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teachConnectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the teachConnectApp
+ */
+angular.module('teachConnectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teachConnectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teachConnectApp
+ */
+angular.module('teachConnectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
